b8bbb7bb5099990ccc420fca14e6c559ed0d1c13
Tiago Angelo <kurtis.angelo@gmail.com>
2021-02-12 07:42:55 -0300

    Making URL configurable

--- client_options.go
+++ client_options.go
@@ -2,3 +2,10 @@ package tyk
 
 // ClientOptionFunc can be used customize a new Tyk API client.
 type ClientOptionFunc func(*Client) error
+
+// WithBaseURL sets the base URL for API requests to a custom endpoint.
+func WithBaseURL(urlStr string) ClientOptionFunc {
+	return func(c *Client) error {
+		return c.setBaseURL(urlStr)
+	}
+}
