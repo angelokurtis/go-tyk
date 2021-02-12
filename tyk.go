7e14aa02834688666386a079ec29c10c2bff7eb3
Tiago Angelo <kurtis.angelo@gmail.com>
2021-02-12 07:40:16 -0300

    Abstraction for easier HTTP requests

--- tyk.go
+++ tyk.go
@@ -1,6 +1,10 @@
 package tyk
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -59,7 +63,7 @@ func (c *Client) setBaseURL(urlStr string) error {
 
 	baseURL, err := url.Parse(urlStr)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to set the base URL: %w", err)
 	}
 
 	if !strings.HasSuffix(baseURL.Path, apiVersionPath) {
@@ -88,4 +92,93 @@ func (c *Client) apply(options ...ClientOptionFunc) error {
 func (c *Client) BaseURL() *url.URL {
 	u := *c.baseURL
 	return &u
+}
+
+func (c *Client) GET(path string, resp interface{}) error {
+	return c.makeRequest("GET", path, resp, nil)
+}
+
+func (c *Client) POST(path string, resp interface{}, payload interface{}) error {
+	return c.makeRequest("POST", path, resp, payload)
+}
+
+func (c *Client) PUT(path string, resp interface{}, payload interface{}) error {
+	return c.makeRequest("PUT", path, resp, payload)
+}
+
+func (c *Client) makeRequest(method, path string, resp interface{}, payload interface{}) error {
+	u := *c.baseURL
+	unescaped, err := url.PathUnescape(path)
+	if err != nil {
+		return fmt.Errorf("failed to make request: %w", err)
+	}
+
+	// Set the encoded path data
+	u.RawPath = c.baseURL.Path + path
+	u.Path = c.baseURL.Path + unescaped
+
+	// Create a request specific headers map.
+	headers := make(http.Header)
+	headers.Set("Accept", "application/json")
+	headers.Set("X-Tyk-Authorization", c.secret)
+
+	if c.UserAgent != "" {
+		headers.Set("User-Agent", c.UserAgent)
+	}
+
+	var body io.Reader
+	if method == "POST" || method == "PUT" {
+		headers.Set("Content-Type", "application/json")
+
+		if payload != nil {
+			b, err := json.Marshal(payload)
+			if err != nil {
+				return fmt.Errorf("failed to make request: %w", err)
+			}
+			body = bytes.NewReader(b)
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), body)
+	if err != nil {
+		return fmt.Errorf("failed to make request: %w", err)
+	}
+
+	// Set the request specific headers.
+	for k, v := range headers {
+		req.Header[k] = v
+	}
+
+	res, err := c.http.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to make request: %w", err)
+	}
+
+	err = checkResponse(res)
+	if err != nil {
+		return err
+	}
+
+	if resp != nil {
+		var eerr error
+		if w, ok := resp.(io.Writer); ok {
+			_, eerr = io.Copy(w, res.Body)
+		} else {
+			eerr = json.NewDecoder(res.Body).Decode(resp)
+		}
+		if eerr != nil {
+			return fmt.Errorf("failed to decode the response: %w", eerr)
+		}
+	}
+
+	return nil
+}
+
+func checkResponse(r *http.Response) error {
+	switch r.StatusCode {
+	case http.StatusOK, http.StatusCreated, http.StatusAccepted, http.StatusNoContent, http.StatusNotModified:
+		return nil
+	}
+
+	return NewResponseErr(r)
 }
